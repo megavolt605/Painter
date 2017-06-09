407d83f0699c100c98383126e9b436885d9ce3b6
Igor Smirnov <s@ronin.pro>
2017-06-09 14:16:42 +0300

    Initial Commit

---
+++ Painter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Painter
+//
+//  Created by Igor Smirnov on 09/06/2017.
+//  Copyright © 2017 Complex Numbers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
